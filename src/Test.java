f2f19be557087a00fc738fde7d4de4826a3e8d9e
Juraj Vlk <juraj.vlk@gmail.com>
2019-01-30 16:00:53 +0100

    cleanup

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,5 @@
-public class Test extends SuperClass {
-
-    public void printMe() {
-        System.out.print("sub context ");
-    }
-
-    /**
-     * main class
-     *
-     * @param argv
-     */
+public class Test {
     public static void main(String argv[]) {
-        SuperClass sc = new Test();
-        sc.printMe();
     }
 
-}
-
-class SuperClass {
-    void printMe() {
-        System.out.print("super context ");
-    }
 }
