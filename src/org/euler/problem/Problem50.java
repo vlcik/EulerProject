d80d538965f2cf58664a8656c947aca530d49a21
Juraj Vlk <jvlk@gohealth.com>
2019-07-19 18:49:14 +0200

    Problem 50

---
+++ src/org/euler/problem/Problem50.java
@@ -0,0 +1,12 @@
+package org.euler.problem;
+
+public class Problem50 {
+
+    public void compute() {
+
+    }
+
+    public static void main(String[] args) {
+        new Problem50().compute();
+    }
+}
